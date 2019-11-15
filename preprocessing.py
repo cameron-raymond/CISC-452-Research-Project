23d4fadf0f0ca6dc2fa56226475fcaf2c7d64a74
Cameron Raymond <15cjkr@queensu.ca>
2019-11-15 18:11:36 -0500

    cleaned up

--- preprocessing.py
+++ preprocessing.py
@@ -10,32 +10,12 @@ nltk.download('wordnet')
 lemmatizer = WordNetLemmatizer()
 
 def text_clean(text):
-    lemmatize = lambda token_list : [ lemmatizer.lemmatize(token) for token in token_list \
-                                    if token not in stopwords.words('english') and len(token) > 3] 
-    # def lemmatize(token_list):
-    #     lemmatize =
-    #     for token in token_list:
-    #         token = lemmatizer.lemmatize(token)
-    #         if token not in stopwords.words('english') and len(token) > 3:
-    #             lemmatize.append(token)
-    #     return lemmatize
-    text = text.lower()
-    token_words=word_tokenize(text)
+    lemmatize   = lambda token_list : \
+                        [ lemmatizer.lemmatize(token) for token in token_list \
+                        if token not in stopwords.words('english') and len(token) > 3] 
+    text        = text.lower()
+    token_words = word_tokenize(text)
     return lemmatize(token_words)
-
-    # cleaned_tweet 	= []
-    # tweet			= tweet_obj._json
-    # raw_text		= emoji.demojize(tweet['full_text'])
-    # cleaned_text 	= p.clean(raw_text)
-    # cleaned_text    = lemmatize(cleaned_text)
-    # cleaned_tweet 	+= [tweet['id'],'tweet', tweet['created_at'],tweet['source'], tweet['full_text'],cleaned_text,tweet['favorite_count'], tweet['retweet_count']]
-    # hashtags = ", ".join([hashtag_item['text'] for hashtag_item in tweet['entities']['hashtags']])
-    # cleaned_tweet.append(hashtags) #append hashtags 
-    # mentions = ", ".join([mention['screen_name'] for mention in tweet['entities']['user_mentions']])
-    # cleaned_tweet.append(mentions) #append mentions
-    # cleaned_tweet.append(tweet['user']['screen_name'])
-    # single_tweet_df = pd.DataFrame([cleaned_tweet], columns=COLS)
-    #return single_tweet_df
 
 
 if __name__ == "__main__":
