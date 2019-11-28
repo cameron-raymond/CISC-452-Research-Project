1c28c4119101d0d1b02c40cbd7dfb79b14f39d8b
Cameron Raymond <15cjkr@queensu.ca>
2019-11-28 14:55:24 -0500

    Reads in all models from saved_models dir directly

--- Demo.py
+++ Demo.py
@@ -1,6 +1,7 @@
-from LSTM import Toxic_Comment_LSTM
 import time
 import sys
+import os
+from LSTM import Toxic_Comment_LSTM
 import numpy as np
 import pandas as pd
 import preprocessing.preprocessing_helpers as preproc
@@ -20,7 +21,7 @@ def validate(model,x_test,y_test,numToValidate=None):
         print('\t* test loss, test acc:', results)
         # Generate predictions (probabilities -- the output of the last layer)
         # on new data using `predict`
-        shuffle_idx = np.random.choice(np.arange(x_test.shape[0]), 50, replace=False)
+        shuffle_idx = np.random.choice(np.arange(x_test.shape[0]), 5, replace=False)
         x_sample, y_sample = x_test[shuffle_idx],y_test[shuffle_idx]
         print('\n--- Generate predictions for {} samples ---'.format(x_sample.shape[0]))
         predictions = model.predict(x_sample).round()
@@ -31,13 +32,28 @@ def validate(model,x_test,y_test,numToValidate=None):
             print("\t\t* targ vec -> {}".format(target))
         print('predictions shape:', predictions.shape)
 
+def return_models(directory):
+    models = []
+    directory = os.fsencode(directory).decode("utf-8") 
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".h5"): 
+            full_path = os.path.join(directory, filename)
+            model = load_model(full_path)
+            models.append((filename,model))
+        else:
+            print("Only keep h5 models in the saved models directory")
+    return models
+
 
 
 if __name__ == "__main__":
-    
+    print("--- starting demo ---")
     numToValidate = int(sys.argv[1]) if len(sys.argv) > 1 and  sys.argv[1].isdigit() else 1000
     start = time.time()
-    model_str = "./saved_models/{}.h5"
+    model_str = "./saved_models/"
+    models = return_models(model_str)
+    print("--- imported {} model(s) ---".format(len(models)))
     print("--- Import training data (used for tokenizing) ---")
     train_df, max_length   = preproc.return_data('./data/{}.csv'.format("cleaned_train"))
     labels = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']    
@@ -56,7 +72,7 @@ if __name__ == "__main__":
     print("--- import test labels ---")
     y_test = pd.read_csv('./data/{}.csv'.format("cleaned_test_labels"))
     y_test = y_test[labels].to_numpy()
-    print("--- loading in LSTM ---")
-    LSTM = load_model(model_str.format("toxic_comment_LSTM"))
     print("--- Validating on {} test documents ---".format(numToValidate))
-    validate(LSTM,x_test,y_test,numToValidate)
+    for model_name, model in models:
+        print("--- Validating {} ---".format(model_name))
+        validate(model,x_test,y_test,numToValidate)
